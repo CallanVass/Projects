c0ae38cb093b225d50f2c9ba93558f48dba5ddf8
Callan <callan.v@fake.com>
2023-11-15 07:48:49 +1100

    Completed ed lesson

--- basic/ed_lesson_create_flask_app.py
+++ basic/ed_lesson_create_flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from datetime import datetime
+import random
 
 app = Flask(__name__)
 
@@ -7,6 +8,11 @@ app = Flask(__name__)
 def display_current_time():
     current_time = datetime.now().strftime("%H:%M")
     return f"<p>{current_time}</p>"
+
+@app.route("/coinflip")
+def coin_flip():
+    coin_list = ["Heads", "Tails"]
+    return { "result": random.choice(coin_list) }
 
 
 if __name__ == '__main__':
