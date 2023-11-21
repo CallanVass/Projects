cf2c1cc5d3d8bdf0b7b12e5695a0453d81f14760
Callan <callan.v@fake.com>
2023-11-21 14:16:41 +1100

    completed lesson on Authentication

--- trello_clone/app.py
+++ trello_clone/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 # Create instance of the app
 app = Flask(__name__)
@@ -12,6 +13,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://trello_dev:callan
 # Import (must be after config but before routes/error handlers)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 
 # Declaring a model to create a table in the database (postgresql) (entity)
 class Card(db.Model):
@@ -28,6 +30,20 @@ class CardSchema(ma.Schema):
     class Meta:
         fields = ("id", "title", "description", "status", "date_created")
 
+# Create user instance
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "email", "password", "is_admin")
+
 # Declaring a cli command
 @app.cli.command("db_create")
 def db_create():
@@ -39,6 +55,21 @@ def db_create():
 
 @app.cli.command("db_seed")
 def db_seed():
+    users = [
+        User(
+            email="admin@spam.com",
+            password=bcrypt.generate_password_hash("spam").decode("utf-8"),
+            is_admin=True
+        ),
+        User(
+            name="John",
+            email="john@spam.com",
+            password=bcrypt.generate_password_hash("tisbutascratch").decode("utf-8"),
+            is_admin=False
+        )
+
+    ]
+
     cards = [
     Card(
         title = "Start the project",
@@ -61,10 +92,29 @@ def db_seed():
     ]
     # Adding transaction to queue
     db.session.add_all(cards)
+    db.session.add_all(users)
     # Commiting transaction
     db.session.commit()
 
     print("Database Seeded")
+
+@app.route("/users/register", methods=["POST"])
+def register():
+    # Parse incoming POST body through the schema (excludes id)
+    user_info = UserSchema(exclude=["id"]).load(request.json)
+    # Create a new user with the parsed data
+    user = User(
+        email=user_info["email"],
+        password=bcrypt.generate_password_hash(user_info["password"]).decode("utf8"),
+        name=user_info.get("name", "")
+    )
+    # Add and commit the new user to the database
+    db.session.add(user)
+    db.session.commit()
+    print(user.__dict__)
+    # Return the new user (exludes password)
+    return UserSchema(exclude=["password"]).dump(user), 201
+
 
 
 @app.cli.command("all_cards")
@@ -82,18 +132,13 @@ def all_cards():
 @app.route("/cards")
 def all_cards():
     # Select * from cards;
-    stmt = db.select(Card).where(db.or_(Card.status != "Done", Card.id > 2)).order_by(Card.title.desc())
-    
-    stmt = db.select(Card)
+    stmt = db.select(Card)#.where(db.or_(Card.status != "Done", Card.id > 2)).order_by(Card.title.desc())
     cards = db.session.scalars(stmt).all()
     return CardSchema(many=True).dump(cards)
     # dump() will convert a Python object into a JSON object, 
     # dumps() encodes a Python object into JSON string, 
     # which makes it more readily readable to more frameworks/languages.
    
-
-
-
 
 
 # Declaring a route
